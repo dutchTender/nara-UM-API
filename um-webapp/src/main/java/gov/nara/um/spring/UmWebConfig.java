cc305d2c6066ebf995de40853926df5d83931ced
Administrator <lzhang421@gmail.com>
2019-11-14 18:38:39 -0500

    added swagger documentation dependencies.

--- um-webapp/src/main/java/gov/nara/um/spring/UmWebConfig.java
+++ um-webapp/src/main/java/gov/nara/um/spring/UmWebConfig.java
@@ -2,29 +2,52 @@ package gov.nara.um.spring;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 @ComponentScan({ "gov.nara.um.web" })
 @EnableWebMvc
+@EnableSwagger2
 public class UmWebConfig implements WebMvcConfigurer {
 
     public UmWebConfig() {
         super();
     }
 
-    // configuration
+    // swagger configuration
+    @Bean
+    public Docket swagConfig(){ // @formatter:off
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+
+                .build()
+                .pathMapping("/api")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                ;
+    }// @formatter:on
 
 
 
+
+
+    // custom http message converter
 
     @Override
     public void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
