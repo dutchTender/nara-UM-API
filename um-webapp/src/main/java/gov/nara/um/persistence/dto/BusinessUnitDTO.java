53a6a1fa7c791c1fc26dc7166d8fc8453d3fc09a
Administrator <lzhang421@gmail.com>
2019-11-22 10:40:28 -0500

    fixed issue with business unit serialization data. need to update business unit controller with busienss_unit DTO to fully address the display issue for all context of the controller.

--- um-webapp/src/main/java/gov/nara/um/persistence/dto/BusinessUnitDTO.java
+++ um-webapp/src/main/java/gov/nara/um/persistence/dto/BusinessUnitDTO.java
@@ -1,5 +1,11 @@
 package gov.nara.um.persistence.dto;
 
+import gov.nara.um.persistence.model.BusinessUnitConfigurationPreference;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class BusinessUnitDTO {
 
     private Integer id;
@@ -10,6 +16,65 @@ public class BusinessUnitDTO {
 
     private String ldap_name;
 
+
+    private List<BusinessUnitConfigPreferenceDTO> businessUnitConfigPreferences = new ArrayList<>();
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOrg_code() {
+        return org_code;
+    }
+
+    public void setOrg_code(String org_code) {
+        this.org_code = org_code;
+    }
+
+    public String getLdap_name() {
+        return ldap_name;
+    }
+
+    public void setLdap_name(String ldap_name) {
+        this.ldap_name = ldap_name;
+    }
+
+    public List<BusinessUnitConfigPreferenceDTO> getBusinessUnitConfigPreferences() {
+        return businessUnitConfigPreferences;
+    }
+
+    public void setBusinessUnitConfigPreferences(List<BusinessUnitConfigPreferenceDTO> businessUnitConfigPreferences) {
+        this.businessUnitConfigPreferences = businessUnitConfigPreferences;
+    }
+
+    public BusinessUnitConfigPreferenceDTO addBusinessUnitConfigPreference(BusinessUnitConfigPreferenceDTO businessUnitConfigPreferenceDTO){
+        businessUnitConfigPreferences.add(businessUnitConfigPreferenceDTO);
+
+        return businessUnitConfigPreferenceDTO;
+    }
+
+    public void removeBusinessUnitConfigPreference(BusinessUnitConfigPreferenceDTO businessUnitConfigPreferenceDTO){
+
+        for(Iterator<BusinessUnitConfigPreferenceDTO> iterBUCP = businessUnitConfigPreferences.iterator(); iterBUCP.hasNext(); ) {
+            BusinessUnitConfigPreferenceDTO current = iterBUCP.next();
+            if(current.equals(businessUnitConfigPreferenceDTO)){
+                iterBUCP.remove();
+            }
+        }
+    }
 
 
 
--- um-webapp/src/main/java/gov/nara/um/spring/controller/BusinessUnitController.java
+++ um-webapp/src/main/java/gov/nara/um/spring/controller/BusinessUnitController.java
@@ -3,7 +3,10 @@ package gov.nara.um.spring.controller;
 import gov.nara.common.util.QueryConstants;
 import gov.nara.common.web.controller.AbstractController;
 import gov.nara.common.web.controller.ISortingController;
+import gov.nara.um.persistence.dto.BusinessUnitConfigPreferenceDTO;
+import gov.nara.um.persistence.dto.BusinessUnitDTO;
 import gov.nara.um.persistence.model.BusinessUnit;
+import gov.nara.um.persistence.model.BusinessUnitConfigurationPreference;
 import gov.nara.um.service.IBusinessUnitService;
 import gov.nara.um.util.UmMappings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -46,14 +51,47 @@ public class BusinessUnitController extends AbstractController<BusinessUnit> imp
     // Unit testing  : NA
     // Integration testing : NA
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    @Override
-    @RequestMapping(params = { QueryConstants.PAGE, QueryConstants.SIZE }, method = RequestMethod.GET)
-    @ResponseBody
+    // sort helper
     public List<BusinessUnit> findAllPaginated(@RequestParam(value = QueryConstants.PAGE) final int page, @RequestParam(value = QueryConstants.SIZE) final int size) {
         return findPaginatedInternal(page, size);
     }
 
+
+
+    @RequestMapping(params = { QueryConstants.PAGE, QueryConstants.SIZE }, method = RequestMethod.GET)
+    @ResponseBody
+    public List<BusinessUnitDTO> findAllPaginatedBU(@RequestParam(value = QueryConstants.PAGE) final int page, @RequestParam(value = QueryConstants.SIZE) final int size) {
+
+
+
+        List<BusinessUnitDTO> returnList =  new ArrayList<>();
+        List<BusinessUnit> rawResultsList =  findPaginatedInternal(page, size);
+        for(Iterator<BusinessUnit> iterBU = rawResultsList.listIterator(); iterBU.hasNext(); ) {
+
+            BusinessUnit currentBU = iterBU.next();
+            BusinessUnitDTO businessUnitDTO = new BusinessUnitDTO();
+            businessUnitDTO.setId(currentBU.getId());
+            businessUnitDTO.setName(currentBU.getName());
+            businessUnitDTO.setOrg_code(currentBU.getOrg_code());
+            businessUnitDTO.setLdap_name(currentBU.getLdapName());
+            List<BusinessUnitConfigurationPreference> preferenceList = currentBU.getBusinessUnitConfigurationPreferences();
+            for(Iterator<BusinessUnitConfigurationPreference> iterBUCP = preferenceList.listIterator(); iterBUCP.hasNext(); ) {
+                BusinessUnitConfigurationPreference currentBUCP = iterBUCP.next();
+                BusinessUnitConfigPreferenceDTO businessUnitConfigPreferenceDTO = new BusinessUnitConfigPreferenceDTO();
+                businessUnitConfigPreferenceDTO.setBusiness_unit_id(currentBUCP.getBusinessUnitID().getId());
+                businessUnitConfigPreferenceDTO.setBusiness_unit_config_id(currentBUCP.getBusinessUnitConfigID().getId());
+                businessUnitConfigPreferenceDTO.setConfiguration_value(currentBUCP.getConfigurationValue());
+
+                businessUnitDTO.addBusinessUnitConfigPreference(businessUnitConfigPreferenceDTO);
+
+
+            }
+            returnList.add(businessUnitDTO);
+
+        }
+
+        return returnList;
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // API
@@ -79,11 +117,45 @@ public class BusinessUnitController extends AbstractController<BusinessUnit> imp
     // Integration testing : NA
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<BusinessUnit> findAll(final HttpServletRequest request) {
         return findAllInternal(request);
     }
+
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<BusinessUnitDTO> findAllBU(final HttpServletRequest request) {
+
+        List<BusinessUnitDTO> returnList =  new ArrayList<>();
+        List<BusinessUnit> rawResultsList =  findAllInternal(request);
+        for(Iterator<BusinessUnit> iterBU = rawResultsList.listIterator(); iterBU.hasNext(); ) {
+
+            BusinessUnit currentBU = iterBU.next();
+            BusinessUnitDTO businessUnitDTO = new BusinessUnitDTO();
+            businessUnitDTO.setId(currentBU.getId());
+            businessUnitDTO.setName(currentBU.getName());
+            businessUnitDTO.setOrg_code(currentBU.getOrg_code());
+            businessUnitDTO.setLdap_name(currentBU.getLdapName());
+            List<BusinessUnitConfigurationPreference> preferenceList = currentBU.getBusinessUnitConfigurationPreferences();
+            for(Iterator<BusinessUnitConfigurationPreference> iterBUCP = preferenceList.listIterator(); iterBUCP.hasNext(); ) {
+                BusinessUnitConfigurationPreference currentBUCP = iterBUCP.next();
+                BusinessUnitConfigPreferenceDTO businessUnitConfigPreferenceDTO = new BusinessUnitConfigPreferenceDTO();
+                businessUnitConfigPreferenceDTO.setBusiness_unit_id(currentBUCP.getBusinessUnitID().getId());
+                businessUnitConfigPreferenceDTO.setBusiness_unit_config_id(currentBUCP.getBusinessUnitConfigID().getId());
+                businessUnitConfigPreferenceDTO.setConfiguration_value(currentBUCP.getConfigurationValue());
+
+                businessUnitDTO.addBusinessUnitConfigPreference(businessUnitConfigPreferenceDTO);
+
+
+            }
+            returnList.add(businessUnitDTO);
+
+        }
+
+        return returnList;
+    }
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // API
