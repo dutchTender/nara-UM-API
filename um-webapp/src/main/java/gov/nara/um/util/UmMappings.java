c5e60ba39490aaac56a36f6394c1eb072342b1d0
Administrator <lzhang421@gmail.com>
2019-11-26 22:46:05 -0500

    added back business unit preferences controller

--- um-webapp/src/main/java/gov/nara/um/util/UmMappings.java
+++ um-webapp/src/main/java/gov/nara/um/util/UmMappings.java
@@ -6,16 +6,9 @@ public final class UmMappings {
 
     public static final String BUSINESSUNITS = "business-units";
 
-    public static final String USERS = "users";
-
-    public static final String USERS_BUSINESSUNITS = "users/business-units";
-
     public static final String BUSINESSUNITS_USERS = "/business-units/users";
 
-
-    public static final String BUSINESSUNITS_CONFIGURATIONS = "/business-units/businessunits-configurations";
-
-    public static final String BUSINESSUNITS_CONFIGURATIONS_PREFERENCES = "/business-units/businessunits-config-preferences";
+    public static final String BUSINESSUNITS_CONFIGURATIONS_PREFERENCES = "/business-units/config-preferences";
     // discoverability
 
     public static final class Singular {
